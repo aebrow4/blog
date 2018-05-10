4c066d28254ffba3437976298fad10d1d6b68e05
aebrow4 <aebrownz11@gmail.com>
2018-05-10 14:51:25 -0700

    remove old image in build script

--- build-prod.sh
+++ build-prod.sh
@@ -7,5 +7,6 @@ cd blog
 
 # stop and remove current container
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=blog:latest --format="{{.ID}}"))
+docker rmi blog:latest
 
 docker build -t blog:latest -f ./Dockerfile . && docker run -d -p 8080:3000 blog:latest
