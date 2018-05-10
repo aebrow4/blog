d0ced44282a9f37053f9d7c0fc6cf75658450551
aebrow4 <aebrownz11@gmail.com>
2018-05-10 12:34:54 -0700

    remove current container during build

--- build-prod.sh
+++ build-prod.sh
@@ -5,5 +5,7 @@ rm -rf blog
 git clone https://github.com/aebrow4/blog.git
 cd blog
 
-# TODO check for unused images and containers and purge them
+# stop and remove current container
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=blog:latest --format="{{.ID}}"))
+
 docker build -t blog:latest -f ./Dockerfile . && docker run -d -p 8080:3000 blog:latest
