bbba9faba0f6874dfad6ea6f2fa3f59c8d929f26
aebrow4 <aebrownz11@gmail.com>
2018-05-13 18:10:13 -0700

    modify dockerfile to build js too

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
-FROM node:carbon
+FROM node
 WORKDIR /root/blog
-
 COPY package*.json ./
 COPY src ./src
 COPY public ./public
 RUN npm install
 RUN npm run build
 
-EXPOSE 3000
-
-# change the command so that it isnt running in development mode
-CMD [ "npm", "start" ]
+FROM nginx
+COPY --from=0 /root/blog/build/index.html /usr/share/nginx/html
+COPY --from=0 /root/blog/build/static /usr/share/nginx/html/static
--- build-prod.sh
+++ build-prod.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-function deploydocker() {
-  # stop and remove current container
-  docker rm $(docker stop $(docker ps -a -q --filter ancestor=blog:latest --format="{{.ID}}"))
-  docker rmi blog:latest
-  
-  docker build -t blog:latest -f ./Dockerfile . && docker run -d -p 8080:3000 blog:latest
-}
-
 function deploynginx() {
   npm install
   npm run build
@@ -17,22 +9,21 @@ function deploynginx() {
   cp -r ~/blog/build/static /var/www/blog/static
 }
 
+function deploynginx-docker() {
+  docker rm $(docker stop $(docker ps -a -q --filter ancestor=blog:latest --format="{{.ID}}"))
+  docker rmi blog:latest
+  
+  docker build -t blog:latest -f ./Dockerfile . && docker run -d -p 8080:80 blog:latest
+}
+
 function main() {
   cd
   rm -rf blog
   git clone https://github.com/aebrow4/blog.git
   cd blog
 
-  if [[ $1 == 'docker' ]]; then
-    deploydocker
-    exit 0
-  elif [[ $1 == 'nginx' ]]; then
-    deploynginx
-    exit 0
-  else
-    echo 'Usage: docker|nginx'
-    exit 1
-  fi
+  deploynginx-docker
+  # or deploynginx
 }
 
-main nginx
+main
